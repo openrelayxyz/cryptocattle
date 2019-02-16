8caa3c515cb2c18306eea796711ecdbb1fd46b90
Connor Bryan <cchromium@gmail.com>
2019-02-15 20:48:52 -0700

    Remove some initial Gatsby crap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,20 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import "semantic-ui-css/semantic.min.css";
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+export default function Layout({ children }) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+      `}
+      render={data => <>{children}</>}
+    />
+  );
 }
-
-export default Layout
---
+++ src/services/index.js
@@ -0,0 +1,1 @@
+export { default as Mock } from "./Mock";
